c409d7dd8012202423793d04d4c67bb88b3ed75f
Prasanna SA <147254363+prasannask17@users.noreply.github.com>
2026-04-20 01:52:46 +0530

    Update Instructions.js

--- SnapNPlate/client/src/components/Instructions.js
+++ SnapNPlate/client/src/components/Instructions.js
@@ -1,33 +1,15 @@
-// import React from "react";
-// import "../styles.css";
-
-// function Instructions({ instructions }) {
-//   return (
-//     <div className="text-start ps-3 mb-5">
-//       <h1>Intructions</h1>
-//       <ul class="list-group list-group-flush ps-5">
-//         {instructions.map((instruction, ind) => (
-//           <li className="list-group-item" key={ind}>
-//             {ind + 1}. {instruction}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default Instructions;
-
 import React from "react";
-import "../App.css";
+import "../styles.css";
 
 function Instructions({ instructions }) {
   return (
-    <div className="detail-section">
-      <h2 className="detail-title">Instructions</h2>
-      <ul className="detail-list">
+    <div className="text-start ps-3 mb-5">
+      <h1>Intructions</h1>
+      <ul class="list-group list-group-flush ps-5">
         {instructions.map((instruction, ind) => (
-          <li key={ind}>{ind + 1}. {instruction}</li>
+          <li className="list-group-item" key={ind}>
+            {ind + 1}. {instruction}
+          </li>
         ))}
       </ul>
     </div>
@@ -35,3 +17,21 @@ function Instructions({ instructions }) {
 }
 
 export default Instructions;
+
+// import React from "react";
+// import "../App.css";
+
+// function Instructions({ instructions }) {
+//   return (
+//     <div className="detail-section">
+//       <h2 className="detail-title">Instructions</h2>
+//       <ul className="detail-list">
+//         {instructions.map((instruction, ind) => (
+//           <li key={ind}>{ind + 1}. {instruction}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Instructions;
