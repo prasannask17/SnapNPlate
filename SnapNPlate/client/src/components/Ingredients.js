9f16db91ab56bdd82b94ef3ab7d0465b4251ab29
Prasanna SA <147254363+prasannask17@users.noreply.github.com>
2026-04-20 01:50:36 +0530

    Update Ingredients.js

--- SnapNPlate/client/src/components/Ingredients.js
+++ SnapNPlate/client/src/components/Ingredients.js
@@ -1,15 +1,33 @@
+// import React from "react";
+// import "../styles.css";
+
+// function Ingredients({ ingredients }) {
+//   return (
+//     <div className="text-start ps-3 mb-5">
+//       <h1>Ingredients</h1>
+//       <ul class="list-group list-group-flush ps-5">
+//         {ingredients.map((ingredient, ind) => (
+//           <li className="list-group-item" key={ind}>
+//             {`${ind + 1}. ${ingredient["name"]} - ${ingredient["qty"]}`}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Ingredients;
+
 import React from "react";
-import "../styles.css";
+import "../App.css";
 
 function Ingredients({ ingredients }) {
   return (
-    <div className="text-start ps-3 mb-5">
-      <h1>Ingredients</h1>
-      <ul class="list-group list-group-flush ps-5">
+    <div className="detail-section">
+      <h2 className="detail-title">Ingredients</h2>
+      <ul className="detail-list">
         {ingredients.map((ingredient, ind) => (
-          <li className="list-group-item" key={ind}>
-            {`${ind + 1}. ${ingredient["name"]} - ${ingredient["qty"]}`}
-          </li>
+          <li key={ind}>{ind + 1}. {ingredient["name"]} — {ingredient["qty"]}</li>
         ))}
       </ul>
     </div>
