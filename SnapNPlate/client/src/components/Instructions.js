5af4b5785b62b9772f980f885cd17a9535fce62f
Prasanna SA <147254363+prasannask17@users.noreply.github.com>
2026-04-20 20:41:20 +0530

    Update Instructions.js

--- SnapNPlate/client/src/components/Instructions.js
+++ SnapNPlate/client/src/components/Instructions.js
@@ -4,7 +4,7 @@ import "../styles.css";
 function Instructions({ instructions }) {
   return (
     <div className="text-start ps-3 mb-5">
-      <h1>Intructions</h1>
+      <h1>Instructions</h1>
       <ul class="list-group list-group-flush ps-5">
         {instructions.map((instruction, ind) => (
           <li className="list-group-item" key={ind}>
